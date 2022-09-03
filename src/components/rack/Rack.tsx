1872927b9c8483bc2b4b0154564cb210e4b5c90a
Olly Rowe <oliver.rowe101@gmail.com>
2022-09-03 19:49:21 +0100

    feat: implement basic rack component

--- src/components/rack/Rack.tsx
+++ src/components/rack/Rack.tsx
@@ -1,16 +1,32 @@
 import React from "react";
 import styled from "styled-components";
+import Tile from "../grid/Tile";
+import { NO_OF_LETTERS } from "../../misc/constants";
 
-interface Props {
-  children?: React.ReactNode;
-}
-
-const Rack: React.FC<Props> = ({ children }) => {
-  return <Container>{children}</Container>;
+const Grid: React.FC = () => {
+  return (
+    <Background>
+      <Container>
+        {new Array(NO_OF_LETTERS).fill(0).map((_, index) => (
+          <Tile key={index} />
+        ))}
+      </Container>
+    </Background>
+  );
 };
+
+const Background = styled.div`
+  display: flex;
+  padding: 8px 12px;
+  flex-grow: 1;
+  overflow: hidden;
+`;
 
 const Container = styled.div`
   display: flex;
+  max-width: 350px;
+  overflow-x: auto;
+  margin: auto;
 `;
 
-export default Rack;
+export default Grid;
