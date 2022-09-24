78bff6679c3e9537bc7b9247a4fe81fff7b6ba9e
Olly Rowe <oliver.rowe101@gmail.com>
2022-09-24 22:09:02 +0100

    feat: improve tile styles and transitions

--- src/components/tile/Tile.tsx
+++ src/components/tile/Tile.tsx
@@ -29,8 +29,14 @@ export const Tile: React.FC<Props> = ({
 
   const { getNewIndex } = useContext(GameContext);
 
-  const { attributes, listeners, transform, transition, setNodeRef } =
-    useSortable({ id, getNewIndex, disabled });
+  const {
+    attributes,
+    listeners,
+    transform,
+    transition,
+    isDragging,
+    setNodeRef,
+  } = useSortable({ id, getNewIndex, disabled });
 
   const getTileColor = () => {
     switch (state) {
@@ -49,6 +55,7 @@ export const Tile: React.FC<Props> = ({
         ref={setNodeRef}
         size={size}
         isBlank={!letter}
+        isDragging={isDragging}
         color={getTileColor()}
         transform={transform}
         transition={transition}
@@ -75,13 +82,15 @@ interface PlaceholderProps {
 const Placeholder = styled.div<PlaceholderProps>`
   box-shadow: ${(props) => props.visible && "inset 0px 0px 0px 2px #d3d6da"};
   margin: 2.5px;
-  width: ${(props) => placeholderSize[props.size]}px;
-  height: ${(props) => placeholderSize[props.size]}px;
+  width: ${(props) => tileSize[props.size]}px;
+  height: ${(props) => tileSize[props.size]}px;
+  border-radius: 4px;
 `;
 
 interface BoxProps {
   size: TileSize;
   isBlank: boolean;
+  isDragging?: boolean;
   color?: string;
   transform: Transform | null;
   transition?: string;
@@ -97,30 +106,30 @@ export const Box = styled.div<BoxProps>`
   font-size: ${(props) => fontSize[props.size]};
   font-weight: bold;
   background-color: ${(props) => props.color || props.theme.border};
-  border: 2px solid ${(props) => props.color || props.theme.border};
-  border-radius: 2px;
+  border-radius: 4px;
   width: ${(props) => tileSize[props.size]}px;
-  height: ${(props) => tileSize[props.size]}px;
+  height: ${(props) => tileSize[props.size] - 5}px;
+  padding-bottom: 5px;
   user-select: none;
   flex-shrink: 0;
   opacity: ${(props) => (props.isBlank ? 0 : 1)};
   transform: ${(props) => CSS.Transform.toString(props.transform)};
-  transition: ${(props) => props.transition};
+  transition: ${(props) => props.transition && props.transition + ","}
+    background-color 1s cubic-bezier(0.19, 1, 0.22, 1);
+  box-shadow: ${(props) =>
+      props.isDragging &&
+      "2px 4px 10px rgba(0, 0, 0, 0.15), 0px 1px 2px rgba(0, 0, 0, 0.25), "}
+    inset 0px -5px 0px 0px rgba(0, 0, 0, 0.1);
 `;
 
-type SizeMapping = { [size in TileSize]: string | number };
+type SizeMapping<T> = { [size in TileSize]: T };
 
-const fontSize: SizeMapping = {
+const fontSize: SizeMapping<string> = {
   small: "x-large",
   regular: "xx-large",
 };
 
-const placeholderSize: SizeMapping = {
+const tileSize: SizeMapping<number> = {
   small: 37,
-  regular: 65,
-};
-
-const tileSize: SizeMapping = {
-  small: 33,
-  regular: 61,
+  regular: 62,
 };
