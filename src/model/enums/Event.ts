107c3bd7813568ab4d288802cdf51486604c4e2f
Olly Rowe <oliver.rowe101@gmail.com>
2022-10-21 22:14:19 +0100

    feat: add first day of pride month event

--- src/model/enums/Event.ts
+++ src/model/enums/Event.ts
@@ -6,6 +6,7 @@ export enum Event {
   ST_PATRICKS_DAY = "St. Patrick's Day",
   APRIL_FOOLS = "April Fools' Day",
   EASTER_SUNDAY = "Easter Sunday",
+  FIRST_DAY_OF_PRIDE_MONTH = "First Day of Pride Month",
   HALLOWEEN = "Halloween",
   FIRST_DAY_OF_HANUKKAH = "First Day of Hanukkah",
   CHRISTMAS = "Christmas",
