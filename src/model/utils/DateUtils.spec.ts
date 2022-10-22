84816904e892e637abd72890bd8ca3d4e902bc13
Olly Rowe <oliver.rowe101@gmail.com>
2022-10-22 11:50:21 +0100

    feat: add date utility module

---
+++ src/model/utils/DateUtils.spec.ts
@@ -0,0 +1,227 @@
+import { Event } from "../enums/Event";
+import { DateUtils } from "./DateUtils";
+
+describe("DateUtils", () => {
+  it("identifies an event on a given day", () => {
+    // Loop through each date and check that the correct event is returned
+    Object.entries(dates).forEach(([eventName, eventDates]) => {
+      eventDates.forEach((eventDate) => {
+        const event = DateUtils.getEvent(eventDate);
+
+        if (eventName === Event.FIRST_DAY_OF_HANUKKAH) {
+          // If Hanukkah falls on the same day as Christmas, then prioritise Christmas
+          if (
+            dates[Event.CHRISTMAS].find((date) =>
+              DateUtils.isSameDay(date, eventDate)
+            )
+          ) {
+            // eslint-disable-next-line jest/no-conditional-expect
+            return expect(event).toBe(Event.CHRISTMAS);
+          }
+        }
+
+        expect(event).toBe(eventName);
+      });
+    });
+  });
+
+  it("knows when New Year's Day is", () => {
+    // Each valid date should return true
+    dates[Event.NEW_YEARS_DAY].forEach((date) => {
+      expect(DateUtils.isNewYearsDay(date)).toBe(true);
+    });
+
+    // Non event day should return false
+    expect(DateUtils.isNewYearsDay(nonEventDay)).toBe(false);
+  });
+
+  it("knows when the Chinese New Year is", () => {
+    // Each valid date should return true
+    dates[Event.CHINESE_NEW_YEAR].forEach((date) => {
+      expect(DateUtils.isChineseNewYear(date)).toBe(true);
+    });
+
+    // Non event day should return false
+    expect(DateUtils.isChineseNewYear(nonEventDay)).toBe(false);
+  });
+
+  it("knows when Valentine's Day is", () => {
+    // Each valid date should return true
+    dates[Event.VALENTINES_DAY].forEach((date) => {
+      expect(DateUtils.isValentinesDay(date)).toBe(true);
+    });
+
+    // Non event day should return false
+    expect(DateUtils.isValentinesDay(nonEventDay)).toBe(false);
+  });
+
+  it("knows when Shrove Tuesday is", () => {
+    // Each valid date should return true
+    dates[Event.SHROVE_TUESDAY].forEach((date) => {
+      expect(DateUtils.isShroveTuesday(date)).toBe(true);
+    });
+
+    // Non event day should return false
+    expect(DateUtils.isShroveTuesday(nonEventDay)).toBe(false);
+  });
+
+  it("knows when St. Patrick's Day is", () => {
+    // Each valid date should return true
+    dates[Event.ST_PATRICKS_DAY].forEach((date) => {
+      expect(DateUtils.isStPatricksDay(date)).toBe(true);
+    });
+
+    // Non event day should return false
+    expect(DateUtils.isStPatricksDay(nonEventDay)).toBe(false);
+  });
+
+  it("knows when April Fools' Day is", () => {
+    // Each valid date should return true
+    dates[Event.APRIL_FOOLS].forEach((date) => {
+      expect(DateUtils.isAprilFools(date)).toBe(true);
+    });
+
+    // Non event day should return false
+    expect(DateUtils.isAprilFools(nonEventDay)).toBe(false);
+  });
+
+  it("knows when Easter Sunday is", () => {
+    // Each valid date should return true
+    dates[Event.EASTER_SUNDAY].forEach((date) => {
+      expect(DateUtils.isEasterSunday(date)).toBe(true);
+    });
+
+    // Non event day should return false
+    expect(DateUtils.isEasterSunday(nonEventDay)).toBe(false);
+  });
+
+  it("knows when the first day of pride month is", () => {
+    // Each valid date should return true
+    dates[Event.FIRST_DAY_OF_PRIDE_MONTH].forEach((date) => {
+      expect(DateUtils.isFirstDayOfPrideMonth(date)).toBe(true);
+    });
+
+    // Non event day should return false
+    expect(DateUtils.isFirstDayOfPrideMonth(nonEventDay)).toBe(false);
+  });
+
+  it("knows when Halloween is", () => {
+    // Each valid date should return true
+    dates[Event.HALLOWEEN].forEach((date) => {
+      expect(DateUtils.isHalloween(date)).toBe(true);
+    });
+
+    // Non event day should return false
+    expect(DateUtils.isHalloween(nonEventDay)).toBe(false);
+  });
+
+  it("knows when the first day of Hanukkah is", () => {
+    // Each valid date should return true
+    dates[Event.FIRST_DAY_OF_HANUKKAH].forEach((date) => {
+      expect(DateUtils.isFirstDayOfHanukkah(date)).toBe(true);
+    });
+
+    // Non event day should return false
+    expect(DateUtils.isFirstDayOfHanukkah(nonEventDay)).toBe(false);
+  });
+
+  it("knows when Christmas Day is", () => {
+    // Each valid date should return true
+    dates[Event.CHRISTMAS].forEach((date) => {
+      expect(DateUtils.isChristmas(date)).toBe(true);
+    });
+
+    // Non event day should return false
+    expect(DateUtils.isChristmas(nonEventDay)).toBe(false);
+  });
+
+  it("knows whether two dates fall on the same day", () => {
+    expect(DateUtils.isSameDay(today, alsoToday)).toBe(true);
+    expect(DateUtils.isSameDay(today, tomorrow)).toBe(false);
+  });
+
+  it("can calculate the number of dates between two dates", () => {
+    expect(DateUtils.getDaysBetween(today, alsoToday)).toBe(0);
+    expect(DateUtils.getDaysBetween(today, tomorrow)).toBe(1);
+    expect(DateUtils.getDaysBetween(today, tenDaysTime)).toBe(10);
+  });
+});
+
+// The dates of each event for the next five years
+const dates: Record<Event, Date[]> = {
+  "New Year's Day": [
+    new Date(2022, 0, 1),
+    new Date(2023, 0, 1),
+    new Date(2024, 0, 1),
+    new Date(2025, 0, 1),
+  ],
+  "Chinese New Year": [
+    new Date(2022, 1, 1),
+    new Date(2023, 0, 22),
+    new Date(2024, 1, 10),
+    new Date(2025, 0, 29),
+  ],
+  "Valentine's Day": [
+    new Date(2022, 1, 14),
+    new Date(2023, 1, 14),
+    new Date(2024, 1, 14),
+    new Date(2025, 1, 14),
+  ],
+  "Shrove Tuesday": [
+    new Date(2022, 2, 1),
+    new Date(2023, 1, 21),
+    new Date(2024, 1, 13),
+    new Date(2025, 2, 4),
+  ],
+  "St. Patrick's Day": [
+    new Date(2022, 2, 17),
+    new Date(2023, 2, 17),
+    new Date(2024, 2, 17),
+    new Date(2025, 2, 17),
+  ],
+  "April Fools' Day": [
+    new Date(2022, 3, 1),
+    new Date(2023, 3, 1),
+    new Date(2024, 3, 1),
+    new Date(2025, 3, 1),
+  ],
+  "Easter Sunday": [
+    new Date(2022, 3, 17),
+    new Date(2023, 3, 9),
+    new Date(2024, 2, 31),
+    new Date(2025, 3, 20),
+  ],
+  "First Day of Pride Month": [
+    new Date(2022, 5, 1),
+    new Date(2023, 5, 1),
+    new Date(2024, 5, 1),
+    new Date(2025, 5, 1),
+  ],
+  "First Day of Hanukkah": [
+    new Date(2022, 11, 18),
+    new Date(2023, 11, 7),
+    new Date(2024, 11, 25),
+    new Date(2025, 11, 14),
+  ],
+  Halloween: [
+    new Date(2022, 9, 31),
+    new Date(2023, 9, 31),
+    new Date(2024, 9, 31),
+    new Date(2024, 9, 31),
+  ],
+  Christmas: [
+    new Date(2022, 11, 25),
+    new Date(2023, 11, 25),
+    new Date(2024, 11, 25),
+    new Date(2025, 11, 25),
+  ],
+};
+
+// A day which has no event
+const nonEventDay = new Date(2002, 2, 20);
+
+// Other dates
+const today = new Date();
+const alsoToday = new Date(today.getTime());
+const tomorrow = new Date(today.getTime() + 24 * 60 * 60 * 1000);
+const tenDaysTime = new Date(today.getTime() + 10 * 24 * 60 * 60 * 1000);
---
+++ src/model/utils/DateUtils.ts
@@ -0,0 +1,250 @@
+import { Event } from "../enums/Event";
+
+/**
+ * Date Utils.
+ *
+ * Utility which supports the identification of various event days
+ * within the year.
+ *
+ * Note that the methods contained within this class use the user's
+ * local date and time settings.
+ */
+export class DateUtils {
+  /** The number of milliseconds in a day */
+  private static MILLISECONDS_IN_DAY = 24 * 60 * 60 * 1000;
+
+  /**
+   * Gets the event, if any, which falls on the specified date.
+   *
+   * @param date - the date to identify the event for.
+   * @returns the event on the day, if there is one.
+   */
+  public static getEvent(date: Date) {
+    if (DateUtils.isNewYearsDay(date)) {
+      return Event.NEW_YEARS_DAY;
+    }
+
+    if (DateUtils.isChineseNewYear(date)) {
+      return Event.CHINESE_NEW_YEAR;
+    }
+
+    if (DateUtils.isValentinesDay(date)) {
+      return Event.VALENTINES_DAY;
+    }
+
+    if (DateUtils.isShroveTuesday(date)) {
+      return Event.SHROVE_TUESDAY;
+    }
+
+    if (DateUtils.isStPatricksDay(date)) {
+      return Event.ST_PATRICKS_DAY;
+    }
+
+    if (DateUtils.isAprilFools(date)) {
+      return Event.APRIL_FOOLS;
+    }
+
+    if (DateUtils.isEasterSunday(date)) {
+      return Event.EASTER_SUNDAY;
+    }
+
+    if (DateUtils.isFirstDayOfPrideMonth(date)) {
+      return Event.FIRST_DAY_OF_PRIDE_MONTH;
+    }
+
+    if (DateUtils.isHalloween(date)) {
+      return Event.HALLOWEEN;
+    }
+
+    if (DateUtils.isChristmas(date)) {
+      return Event.CHRISTMAS;
+    }
+
+    if (DateUtils.isFirstDayOfHanukkah(date)) {
+      return Event.FIRST_DAY_OF_HANUKKAH;
+    }
+  }
+
+  public static isNewYearsDay(date: Date) {
+    return date.getDate() === 1 && date.getMonth() === 0;
+  }
+
+  public static isChineseNewYear(date: Date) {
+    const chineseDate = DateUtils.getChineseDate(date);
+
+    return chineseDate.day === 1 && chineseDate.month === 1;
+  }
+
+  public static isValentinesDay(date: Date) {
+    return date.getDate() === 14 && date.getMonth() === 1;
+  }
+
+  public static isShroveTuesday(date: Date) {
+    const easter = DateUtils.getEaster(date);
+
+    const shroveTuesday = new Date(easter);
+
+    shroveTuesday.setDate(shroveTuesday.getDate() - 47);
+
+    return (
+      date.getDate() === shroveTuesday.getDate() &&
+      date.getMonth() === shroveTuesday.getMonth()
+    );
+  }
+
+  public static isStPatricksDay(date: Date) {
+    return date.getDate() === 17 && date.getMonth() === 2;
+  }
+
+  public static isAprilFools(date: Date) {
+    return date.getDate() === 1 && date.getMonth() === 3;
+  }
+
+  public static isEasterSunday(date: Date) {
+    const easter = DateUtils.getEaster(date);
+
+    return (
+      date.getDate() === easter.getDate() &&
+      date.getMonth() === easter.getMonth()
+    );
+  }
+
+  public static isFirstDayOfPrideMonth(date: Date) {
+    return date.getDate() === 1 && date.getMonth() === 5;
+  }
+
+  public static isHalloween(date: Date) {
+    return date.getDate() === 31 && date.getMonth() === 9;
+  }
+
+  /**
+   * Hanukkah starts on the 25th day of the Hebrew month Kislev.
+   *
+   * As each Hebrew day starts in the evening of what we'd consider a
+   * day, this method uses the 24th day instead of the 25th.
+   */
+  public static isFirstDayOfHanukkah(date: Date) {
+    const hebrewDate = DateUtils.getHebrewDate(date);
+
+    return hebrewDate.day === 24 && hebrewDate.month === 3;
+  }
+
+  public static isChristmas(date: Date) {
+    return date.getDate() === 25 && date.getMonth() === 11;
+  }
+
+  /**
+   * Determines whether two days fall on the same day.
+   *
+   * @param firstDate - the first date to compare.
+   * @param secondDate - the second date to compare.
+   * @returns boolean indicating whether the two dates represent the same day.
+   */
+  public static isSameDay(firstDate: Date, secondDate: Date) {
+    return (
+      firstDate.getDate() === secondDate.getDate() &&
+      firstDate.getMonth() === secondDate.getMonth() &&
+      firstDate.getFullYear() === secondDate.getFullYear()
+    );
+  }
+
+  /**
+   * Identifies the total number of days between two specified dates.
+   *
+   * @param startDate - the start date.
+   * @param endDate - the end date.
+   * @returns total number of days between the two dates.
+   */
+  public static getDaysBetween(startDate: Date, endDate: Date) {
+    // Standardise the dates and convert to milliseconds
+    const startDateInMilliseconds = startDate.getTime();
+    const endDateInMilliseconds = endDate.getTime();
+
+    const timeDifferent = endDateInMilliseconds - startDateInMilliseconds;
+
+    return Math.floor(timeDifferent / DateUtils.MILLISECONDS_IN_DAY);
+  }
+
+  /**
+   * Returns the date of easter for a given year.
+   *
+   * Based on the gist https://gist.github.com/johndyer/0dffbdd98c2046f41180c051f378f343.
+   *
+   * @param date - the date to obtain the current year from.
+   * @returns the date of easter.
+   */
+  private static getEaster(date: Date) {
+    const year = date.getFullYear();
+
+    const f = Math.floor;
+    // Golden Number - 1
+    const G = year % 19;
+    const C = f(year / 100);
+    // Related to Epact
+    const H = (C - f(C / 4) - f((8 * C + 13) / 25) + 19 * G + 15) % 30;
+    // Number of days from 21 March to the Paschal full moon
+    const I = H - f(H / 28) * (1 - f(29 / (H + 1)) * f((21 - G) / 11));
+    // Weekday for the Paschal full moon
+    const J = (year + f(year / 4) + I + 2 - C + f(C / 4)) % 7;
+    // Number of days from 21 March to the Sunday on or before the Paschal full moon
+    const L = I - J;
+
+    const month = 3 + f((L + 40) / 44);
+    const day = L + 28 - 31 * f(month / 4);
+
+    return new Date(year, month - 1, day);
+  }
+
+  /**
+   * Converts a specified date into a corresponding day and month on
+   * the Hebrew calendar.
+   *
+   * @param date - the date to convert.
+   * @returns the Hebrew day and month.
+   */
+  private static getHebrewDate(date: Date) {
+    const day = new Intl.DateTimeFormat(HEBREW_LOCALE, {
+      day: "numeric",
+    }).format(date);
+    const month = new Intl.DateTimeFormat(HEBREW_LOCALE, {
+      month: "numeric",
+    }).format(date);
+    const year = new Intl.DateTimeFormat(HEBREW_LOCALE, {
+      year: "numeric",
+    }).format(date);
+
+    return {
+      day: parseInt(day),
+      month: parseInt(month),
+      year: parseInt(year),
+    };
+  }
+
+  /**
+   * Converts a specified date into a corresponding day and month on
+   * the Chinese calendar.
+   *
+   * @param date - the date to convert.
+   * @returns the Hebrew day and month.
+   */
+  private static getChineseDate(date: Date) {
+    const day = new Intl.DateTimeFormat(CHINESE_LOCALE, {
+      day: "numeric",
+    }).format(date);
+    const month = new Intl.DateTimeFormat(CHINESE_LOCALE, {
+      month: "numeric",
+    }).format(date);
+    const year = new Intl.DateTimeFormat(CHINESE_LOCALE, {
+      year: "numeric",
+    }).format(date);
+
+    return {
+      day: parseInt(day),
+      month: parseInt(month),
+      year: parseInt(year),
+    };
+  }
+}
+
+const HEBREW_LOCALE = "en-u-ca-hebrew";
+const CHINESE_LOCALE = "en-u-ca-chinese";
