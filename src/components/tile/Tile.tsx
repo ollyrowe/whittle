7ace71b30e2b2516cf21bc219f330ecf8b5aaf9b
Olly Rowe <oliver.rowe101@gmail.com>
2022-10-23 19:50:21 +0100

    perf: rework tile to prevent excess class generation

--- src/components/tile/Tile.tsx
+++ src/components/tile/Tile.tsx
@@ -3,7 +3,7 @@ import { useTheme } from "@mui/material";
 import styled from "styled-components";
 import { useConditionTimer } from "../../hooks/useConditionTimer";
 import { useSortable } from "@dnd-kit/sortable";
-import { CSS, Transform } from "@dnd-kit/utilities";
+import { CSS } from "@dnd-kit/utilities";
 import { GameContext } from "../providers/GameProvider";
 import { TileState } from "../../model/enums/TileState";
 import { Letter } from "../../model/enums/Letter";
@@ -61,6 +61,12 @@ export const Tile: React.FC<Props> = ({
   // Whether the sortable transformation should be applied to the tile
   const applyTransformation = isDragging || !hasLetter || hasOverTimerLapsed;
 
+  // Transformation styles to be applied to the draggable element
+  const style = {
+    transform: CSS.Transform.toString(applyTransformation ? transform : null),
+    transition,
+  };
+
   const getTileColor = () => {
     switch (state) {
       case TileState.CORRECT:
@@ -81,8 +87,7 @@ export const Tile: React.FC<Props> = ({
         isDragging={isDragging}
         isOver={isOver}
         color={getTileColor()}
-        transform={applyTransformation ? transform : null}
-        transition={transition}
+        style={style}
         {...attributes}
         {...listeners}
       >
@@ -107,7 +112,8 @@ interface PlaceholderProps {
  * Placeholder div which applies an inset border around the tile.
  */
 const Placeholder = styled.div<PlaceholderProps>`
-  box-shadow: ${(props) => props.visible && "inset 0px 0px 0px 2px " + props.theme.palette.border };
+  box-shadow: ${(props) =>
+    props.visible && "inset 0px 0px 0px 2px " + props.theme.palette.border};
   margin: 2.5px;
   width: ${(props) => tileSize[props.size]}px;
   height: ${(props) => tileSize[props.size]}px;
@@ -120,8 +126,6 @@ interface BoxProps {
   isDragging?: boolean;
   isOver?: boolean;
   color?: string;
-  transform?: Transform | null;
-  transition?: string;
 }
 
 /**
@@ -134,7 +138,8 @@ export const Box = styled.div<BoxProps>`
   justify-content: center;
   font-size: ${(props) => fontSize[props.size]};
   font-weight: bold;
-  background-color: ${(props) => props.color || props.theme.palette.tile.default};
+  background-color: ${(props) =>
+    props.color || props.theme.palette.tile.default};
   border-radius: 4px;
   width: ${(props) => tileSize[props.size]}px;
   height: ${(props) => tileSize[props.size]}px;
@@ -142,12 +147,10 @@ export const Box = styled.div<BoxProps>`
   user-select: none;
   flex-shrink: 0;
   opacity: ${(props) => (props.isBlank ? 0 : 1)};
-  transform: ${(props) => CSS.Transform.toString(props.transform || null)};
-  transition: ${(props) => props.transition && props.transition + ","}
     background-color 1s cubic-bezier(0.19, 1, 0.22, 1);
   box-shadow: ${(props) =>
-      props.isDragging &&
-      "2px 4px 10px rgba(0, 0, 0, 0.15), 0px 1px 2px rgba(0, 0, 0, 0.25), "}
+    props.isDragging &&
+    "2px 4px 10px rgba(0, 0, 0, 0.15), 0px 1px 2px rgba(0, 0, 0, 0.25), "}
     inset 0px -5px 0px 0px rgba(0, 0, 0, 0.1);
   z-index: ${(props) =>
     props.isDragging ? 9999 : props.isOver ? 9998 : "auto"};
