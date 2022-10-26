671fdeb13e0e7a058c157392a201ad476d187def
Olly Rowe <oliver.rowe101@gmail.com>
2022-10-26 19:21:09 +0100

    feat: disable reset button when the board is empty

--- src/components/rack/ResetButton.tsx
+++ src/components/rack/ResetButton.tsx
@@ -4,14 +4,20 @@ import { Autorenew as AutorenewIcon } from "@mui/icons-material";
 import { useGameContext } from "../providers/GameProvider";
 
 interface Props {
+  disabled?: boolean;
   className?: string;
 }
 
-const ResetButton: React.FC<Props> = ({ className }) => {
+const ResetButton: React.FC<Props> = ({ disabled, className }) => {
   const game = useGameContext();
 
   return (
-    <IconButton onClick={game.reset} size="small" className={className}>
+    <IconButton
+      onClick={game.reset}
+      size="small"
+      disabled={disabled}
+      className={className}
+    >
       <AutorenewIcon fontSize="small" />
     </IconButton>
   );
