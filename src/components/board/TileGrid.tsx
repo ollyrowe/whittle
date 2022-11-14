5b1268b94afd6b8540a7d6acf09023f5fb8f1755
Olly Rowe <oliver.rowe101@gmail.com>
2022-11-14 21:20:39 +0000

    feat: update disabled board styles

--- src/components/board/TileGrid.tsx
+++ src/components/board/TileGrid.tsx
@@ -19,7 +19,7 @@ export const TileGrid: React.FC<Props> = ({ tiles, tileSize, disabled }) => {
           id={tile.getID()}
           letter={tile.getLetter()}
           draggable={tile.isDraggable() && !disabled}
-          disabled={tile.isDisabled()}
+          disabled={tile.isDisabled() || disabled}
           size={tileSize}
           hasPlaceholder
         />
