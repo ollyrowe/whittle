0943491aec48ed70d574dfb2cb8cd0b676db607d
Olly Rowe <oliver.rowe101@gmail.com>
2022-12-31 00:17:49 +0000

    test: add event e2e tests

---
+++ cypress/e2e/events.cy.ts
@@ -0,0 +1,95 @@
+describe("Events", () => {
+  beforeEach(() => {
+    cy.clock().invoke("restore");
+  });
+
+  it("New Year's Day", () => {
+    cy.clock(new Date(2023, 0, 1));
+
+    cy.reload();
+
+    cy.getByTestID("board").should("have.text", "Happy New Year's Day!");
+  });
+
+  it("Chinese New Year", () => {
+    cy.clock(new Date(2023, 0, 22));
+
+    cy.reload();
+
+    cy.getByTestID("board").should("have.text", "Happy Chinese New Year!");
+  });
+
+  it("Valentine's Day", () => {
+    cy.clock(new Date(2023, 1, 14));
+
+    cy.reload();
+
+    cy.getByTestID("board").should("have.text", "Happy Valentine's Day!");
+  });
+
+  it("Shrove Tuesday", () => {
+    cy.clock(new Date(2023, 1, 21));
+
+    cy.reload();
+
+    cy.getByTestID("board").should("have.text", "Happy Pancake Day!");
+  });
+
+  it("St. Patrick's Day", () => {
+    cy.clock(new Date(2023, 2, 17));
+
+    cy.reload();
+
+    cy.getByTestID("board").should("have.text", "Happy Saint Patrick's Day!");
+  });
+
+  it("April Fools' Day", () => {
+    cy.clock(new Date(2023, 3, 1));
+
+    cy.reload();
+
+    cy.getByTestID("board").should("have.text", "Happy April Fools' Day!");
+  });
+
+  it("Easter Sunday", () => {
+    cy.clock(new Date(2023, 3, 9));
+
+    cy.reload();
+
+    cy.getByTestID("board").should("have.text", "Happy Easter!");
+  });
+
+  it("First Day of Pride Month", () => {
+    cy.clock(new Date(2022, 5, 1));
+
+    cy.reload();
+
+    cy.getByTestID("board").should("have.text", "Happy Pride Month!");
+  });
+
+  it("Halloween", () => {
+    cy.clock(new Date(2022, 9, 31));
+
+    cy.reload();
+
+    cy.getByTestID("board").should("have.text", "Happy Halloween!");
+  });
+
+  it("First Day of Hanukkah", () => {
+    cy.clock(new Date(2022, 11, 18));
+
+    cy.reload();
+
+    cy.getByTestID("board").should("have.text", "Happy Hanukkah!");
+  });
+
+  it("Christmas", () => {
+    cy.clock(new Date(2022, 11, 25));
+
+    cy.reload();
+
+    cy.getByTestID("board").should("have.text", "Merry Christmas!");
+  });
+});
+
+export {};
